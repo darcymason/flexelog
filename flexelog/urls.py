b7b77708f1f5d80d45554d55ffe5ebd40d7423c4
Darcy Mason <darcymason@gmail.com>
2024-12-18 18:16:27 -0500

    Skeleton for index logbooks listing with elog.css

--- flexelog/urls.py
+++ flexelog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "flexelog"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:entry_id>", views.detail, name="detail"),
+    path("<str:lb>/<int:entry_id>", views.detail, name="detail"),
 ]
--- flexelog/views.py
+++ flexelog/views.py
@@ -1,18 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from .models import logbook_names
+from .models import logbook_names, Entry
 from django.http import HttpResponse
 
+class Logbook:
+    def __init__(self, name, comment):
+        self.name = name
+        self.comment = comment
 
 
 def index(request):
+
+    logbooks = [
+        Logbook(lb, f"Comment for {lb}")
+        for lb in logbook_names()
+    ]
     context = {
-        "lb_list": logbook_names()
+        "logbooks": logbooks
     }
+
     return render(request, "flexelog/index.html", context)
 
 
-def detail(request, entry_id):
-    return HttpResponse(f"You're looking at entry {entry_id}")
+def detail(request, lb, entry_id):
+    entry = get_object_or_404(Entry, lb=lb, id=entry_id)
+
+    return render(request, "flexelog/detail.html", {"entry": entry})
 
