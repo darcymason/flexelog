cea19d3f6b22d4d5b60c43dba3f34d7498e54266
Darcy Mason <darcymason@gmail.com>
2025-05-24 15:59:44 -0400

    Get New allowed from logbook page * change lb_name to logbook.name most places

--- flexelog/urls.py
+++ flexelog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/do_logout", views.do_logout, name="do_logout"),
     path("", views.index, name="index"),
-    path("<str:lb_name>/", views.logbook_or_new_edit_delete_post, name="logbook"),
+    path("<str:lb_name>/", views.logbook_view, name="logbook"),
     path("<str:lb_name>/<int:entry_id>/", views.entry_detail, name="entry_detail"),
     path("test/<str:lb_name>/<int:entry_id>/", views.test, name="test"),
 ]
